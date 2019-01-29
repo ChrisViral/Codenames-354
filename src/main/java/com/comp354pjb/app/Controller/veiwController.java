f82d53bf3701dd72de679ac9dfb52d1d7dc6ba51
Unknown <shachna@gmail.com>
2019-01-29 01:34:38 -0500

    Added board.fxml and related architecture
    There's also some .idea config crap that went on in order for me to use the interpreter and the sqlite package.

---
+++ src/main/java/com/comp354pjb/app/Controller/veiwController.java
@@ -0,0 +1,14 @@
+package com.comp354pjb.app.Controller;
+import javafx.fxml.FXML;
+
+import javax.xml.soap.Text;
+import javafx.event.ActionEvent;
+
+public class veiwController
+{
+    @FXML private Text word11;
+    @FXML protected void someFunction(ActionEvent actionEvent)
+    {
+        //do some stuff here
+    }
+}
