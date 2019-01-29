9f62488f4dbe9a604f5fb7fdebf4b8f9d37c09a6
Benjamin Therien <benjamintherien@yahoo.ca>
2019-01-28 23:05:43 -0500

    added database files

--- src/main/java/com/comp354pjb/app/App.java
+++ src/main/java/com/comp354pjb/app/App.java
@@ -1,4 +1,5 @@
 package com.comp354pjb.app;
+import com.comp354pjb.app.Model.databaseHelpers;
 import com.comp354pjb.app.Controller.gameController;
 
 /**
@@ -7,15 +8,21 @@ import com.comp354pjb.app.Controller.gameController;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
 
+        try{
+            Class.forName("org.sqlite.JDBC");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
         System.out.println( "Hello World!" );
+        databaseHelpers.connect();
+
 
         //at this point we need our pull method from database to retrieve all cards
-
-
-
 
     }
 }
