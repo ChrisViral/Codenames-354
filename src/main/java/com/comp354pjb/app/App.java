3f9b49ec1964c3ecf61bd957b2eb14643ff94b71
Christophe Savard <christophe_savard@hotmail.ca>
2019-01-29 13:47:00 -0500

    Make project load up GUI

--- src/main/java/com/comp354pjb/app/App.java
+++ src/main/java/com/comp354pjb/app/App.java
@@ -1,28 +1,40 @@
 package com.comp354pjb.app;
+
 import com.comp354pjb.app.Model.databaseHelpers;
-import com.comp354pjb.app.Controller.gameController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.IOException;
+
+public class App extends Application
 {
-
     public static void main( String[] args )
     {
-
-        try{
+        try
+        {
             Class.forName("org.sqlite.JDBC");
-        }catch(Exception e){
+        }
+        catch(Exception e)
+        {
             System.out.println(e);
         }
 
         System.out.println( "Hello World!" );
         databaseHelpers.connect();
 
+        launch(args);
+    }
 
-        //at this point we need our pull method from database to retrieve all cards
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        Scene scene = new Scene(FXMLLoader.<Parent>load(getClass().getResource("Resources/board.fxml")), 500, 400);
 
+        stage.setTitle("Codenames");
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/com/comp354pjb/app/Model/databaseHelpers.java
+++ src/main/java/com/comp354pjb/app/Model/databaseHelpers.java
@@ -7,43 +7,56 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class databaseHelpers {
+public class databaseHelpers
+{
+    public static void connect()
+    {
+        Connection conn = null;
+        try
+        {
 
-public static void connect() {
-    Connection conn = null;
-    try {
-    	
-    	Path currentDir = Paths.get("db/database.db");
-    	System.out.println(currentDir.toAbsolutePath());
-    	String path= currentDir.toAbsolutePath()+ "";
-    	
-        String url = "jdbc:sqlite:"+path;
-        // create a connection to the database
-        conn = DriverManager.getConnection(url);
-        
-        System.out.println("Connection to SQLite has been established.");
-        
-    }  catch (SQLException e) {
-        System.out.println(e.getMessage());
-    } finally {
-        try {
-            if (conn != null) {
-                conn.close();
+            Path currentDir = Paths.get("db/database.db");
+            System.out.println(currentDir.toAbsolutePath());
+            String path = currentDir.toAbsolutePath() + "";
+
+            String url = "jdbc:sqlite:" + path;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (conn != null)
+                {
+                    conn.close();
+                }
             }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+            catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
         }
     }
-}
 
-public static void query(String url, String query)
-{	
-	try (Connection conn = DriverManager.getConnection(url);
-        Statement stmt = conn.createStatement()) {
-    stmt.execute(query);
-	} catch (SQLException e) {
-		System.out.println(e.getMessage());
-	}
-}
+    public static void query(String url, String query)
+    {
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement())
+        {
+            stmt.execute(query);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
