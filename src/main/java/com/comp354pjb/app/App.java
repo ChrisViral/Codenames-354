27eb5e8214f05786396eb5b77fd1167a521484a8
Benjamin Therien <benjamintherien@yahoo.ca>
2019-01-29 14:14:00 -0500

    connected database properly and added methood to get words

--- src/main/java/com/comp354pjb/app/App.java
+++ src/main/java/com/comp354pjb/app/App.java
@@ -1,5 +1,5 @@
 package com.comp354pjb.app;
-import com.comp354pjb.app.Model.databaseHelpers;
+import com.comp354pjb.app.Model.DatabaseHelpers;
 import com.comp354pjb.app.Controller.gameController;
 
 /**
@@ -8,18 +8,17 @@ import com.comp354pjb.app.Controller.gameController;
  */
 public class App 
 {
-
     public static void main( String[] args )
     {
-
-        try{
-            Class.forName("org.sqlite.JDBC");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-
         System.out.println( "Hello World!" );
-        databaseHelpers.connect();
+        DatabaseHelpers.connect();
+        String[] words = DatabaseHelpers.getWords();
+        int count=0;
+        while(count<250)
+        {
+            System.out.println(words[count]);
+            count++;
+        }
 
 
         //at this point we need our pull method from database to retrieve all cards
