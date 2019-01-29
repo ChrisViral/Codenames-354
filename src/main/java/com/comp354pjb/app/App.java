8e2cc4108ac3cfc97613ba141f69e961dd2d860b
Christophe Savard <christophe_savard@hotmail.ca>
2019-01-29 13:50:13 -0500

    Added some comments

--- src/main/java/com/comp354pjb/app/App.java
+++ src/main/java/com/comp354pjb/app/App.java
@@ -9,30 +9,36 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Application entry point class
+ */
 public class App extends Application
 {
+    /**
+     * Application entry point
+     * @param args App arguments
+     */
     public static void main( String[] args )
     {
-        try
-        {
-            Class.forName("org.sqlite.JDBC");
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
-
-        System.out.println( "Hello World!" );
+        //Connect to SQLite database
         databaseHelpers.connect();
 
+        //Launches JavaFX
         launch(args);
     }
 
+    /**
+     * Starts the JavaFX GUI
+     * @param stage JavaFX Stage
+     * @throws IOException FXML file not found
+     */
     @Override
     public void start(Stage stage) throws IOException
     {
+        //Loading FXML file
         Scene scene = new Scene(FXMLLoader.<Parent>load(getClass().getResource("Resources/board.fxml")), 500, 400);
 
+        //Showing GUI
         stage.setTitle("Codenames");
         stage.setScene(scene);
         stage.show();
