190db8504c1d15d309902b03dc59e830cd92aacd
Christophe Savard <christophe_savard@hotmail.ca>
2019-01-29 16:56:36 -0500

    Add back DatabaseHelpers

---
+++ src/main/java/com/comp354pjb/app/Model/DatabaseHelpers.java
@@ -0,0 +1,81 @@
+package com.comp354pjb.app.Model;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class DatabaseHelpers {
+
+    public static String getURL()
+    {
+        //get path to you current root directory, then appends db/codenames.db at the back, and
+        //jbdc:sqlite at the front to complete a correct
+        Path currentDir = Paths.get("db/codenames.db");
+        String path= currentDir.toAbsolutePath()+ "";
+        String url = "jdbc:sqlite:"+path;
+        return url;
+    }
+
+    public static void connect()
+    {
+
+        String url = getURL();
+        Connection conn = null;
+        try
+        {
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (conn != null)
+                {
+                    conn.close();
+                }
+            }
+            catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public static String[] getWords()
+    {
+        String url = getURL();
+        try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement())
+        {
+            //getting the size of the array
+            ResultSet size = stmt.executeQuery("select count(wordValue) as size from word;");
+            String[] words = new String[size.getInt("size")];
+            //getting all the words in the database
+            ResultSet query =  stmt.executeQuery("select * from word;");
+            int count=0;
+            while(query.next())
+            {
+                words[count] = query.getString("wordValue");
+                count++;
+            }
+            return words;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        String[] error= {"error"};
+        return error;
+    }
+
+}
