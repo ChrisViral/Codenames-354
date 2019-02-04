49e7c4f711e2eb02eb15d132cc040d099df7b2dd
Benjamin Therien <benjamintherien@yahoo.ca>
2019-02-04 14:08:46 -0500

    Revert "Implemented Subject on Board Class"
    This reverts commit d675bd5793c9c6f90cf8c3c935664b7dd5a1c071.

--- src/main/java/com/comp354pjb/codenames/model/Subject.java
+++ src/main/java/com/comp354pjb/codenames/model/Subject.java
@@ -2,9 +2,10 @@ package com.comp354pjb.codenames.model;
 
 public interface Subject
 {
-    public void register(Observer obs);
-    public void unregister(Observer obs);
+    public void register(Observer obj);
+    public void unregister(Observer obj);
+
     public void notifyObservers();
 
-    //public Object getUpdate(Observer obs);
+    public Object getUpdate(Observer obj);
 }
