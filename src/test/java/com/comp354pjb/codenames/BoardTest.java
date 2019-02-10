ee8226ff504910a6ca7574a9d70806ff66f04ead
mbwilgus <m.b.wilgus@gmail.com>
2019-02-10 00:15:51 -0500

    Update tests for Board

--- src/test/java/com/comp354pjb/codenames/BoardTest.java
+++ src/test/java/com/comp354pjb/codenames/BoardTest.java
@@ -9,77 +9,45 @@
 
 package com.comp354pjb.codenames;
 
+import com.comp354pjb.codenames.model.DatabaseHelper;
 import com.comp354pjb.codenames.model.board.*;
 import com.comp354pjb.codenames.model.player.PlayerType;
-import com.comp354pjb.codenames.stubs.DatabaseHelperStub;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Random;
 
 public class BoardTest
 {
-    @Test
-    public void createCardsShouldGiveAllCardsAWord() {
-        // mock card set
-        Card[][] cards = mockCardSet();
+    private static final Random RAND = new Random();
 
-        // Do all the cards have a word?
-        for(int i = 0; i < cards.length; i++)
-        {
-            for(int j = 0; j < cards[i].length; j++)
-            {
-                assertEquals("test", cards[i][j].getWord());
-            }
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void createCardsShouldNotAcceptSmallArray() {
+        String[] words = DatabaseHelper.selectWords(RAND.nextInt(24));
+        Card[][] cards = Board.createCards(words, getRandomPlayerType(), new HashSet<>());
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void revealAtShouldNotAcceptBadCoords() {
+        Board board = new Board(DatabaseHelper.selectWords(25), getRandomPlayerType());
+        int x = RAND.nextInt();
+        int y = RAND.nextInt();
+        board.revealAt(x, y);
+    }
+
+    @Test
+    public void boardShouldNotContainNullOrEmpty() {
+        Board board = new Board(DatabaseHelper.selectWords(25), getRandomPlayerType());
+        assertFalse(board.hasWord(null));
+        assertFalse(board.hasWord(""));
+    }
+
+    private PlayerType getRandomPlayerType() {
+        if(RAND.nextInt() % 2 == 0) {
+            return PlayerType.RED;
         }
-    }
-
-    @Test
-    public void getCardShouldNotReturnNull() {
-        String[] words = DatabaseHelperStub.selectWords(25);
-        Board board = mockBoard(words);
-        assertNotNull(board.getCard(0, 0));
-    }
-
-    @Test
-    public void revealAtShouldSetACardAsRevealed() {
-        String[] words = DatabaseHelperStub.selectWords(25);
-        Board board = mockBoard(words);
-
-        // Precondition: card is not revealed.
-        assertFalse(board.getCard(0,0).isRevealed());
-
-        board.revealAt(0, 0);
-
-        assertTrue(board.getCard(0, 0).isRevealed());
-    }
-
-    @Test
-    public void hasWordShouldDetectWhenWordsArePresent() {
-        String[] words = DatabaseHelperStub.selectWords(25);
-        Board board = mockBoard(words);
-        for(int i = 0; i < words.length; i++) {
-            assertTrue(board.hasWord(words[i]));
-        }
-
-        String[] nonDBWords = {"bull", "database", "pear"};
-        for(int i = 0; i < nonDBWords.length; i++) {
-            assertFalse(board.hasWord(nonDBWords[i]));
-        }
-    }
-
-    private static Card[][] mockCardSet() {
-        String[] words = new String[25];
-        Arrays.fill(words, "test");
-        Card[][] cards = Board.createCards(words, PlayerType.RED, new HashSet<>());
-
-        return cards;
-    }
-
-    private static Board mockBoard(String[] words) {
-        Board board = new Board(words, PlayerType.RED);
-        return board;
+        return PlayerType.BLUE;
     }
 }
